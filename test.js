5a5d3fb20e49b3af5f945d47ab3c3734c57ff78f
Titus Wormer <tituswormer@gmail.com>
2017-02-23 17:42:41 +0100

    Update tests for retext@5.0.0

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var repeated = require('./');
 
 test('repeatedWords()', function (t) {
   t.deepEqual(
-    retext().use(repeated).process([
+    retext().use(repeated).processSync([
       'Well, it it doesn’t have to to be. Like a fish in the',
       'the sea.'
     ].join('\n')).messages.map(String),
@@ -19,7 +19,7 @@ test('repeatedWords()', function (t) {
   );
 
   t.deepEqual(
-    retext().use(repeated).process([
+    retext().use(repeated).processSync([
       'LIKE A FISH IN THE',
       'THE SEA.'
     ].join('\n')).messages.map(String),
@@ -28,31 +28,31 @@ test('repeatedWords()', function (t) {
   );
 
   t.deepEqual(
-    retext().use(repeated).process('Duran Duran is awesome.').messages,
+    retext().use(repeated).processSync('Duran Duran is awesome.').messages,
     [],
     'should ignore sentence cased words'
   );
 
   t.deepEqual(
-    retext().use(repeated).process('D. D. will pop up with.').messages,
+    retext().use(repeated).processSync('D. D. will pop up with.').messages,
     [],
     'should ignore initialisms'
   );
 
   t.deepEqual(
-    retext().use(repeated).process('DURAN Duran').messages,
+    retext().use(repeated).processSync('DURAN Duran').messages,
     [],
     'should ignore differently cases words'
   );
 
   t.deepEqual(
-    retext().use(repeated).process('the most heartening exhibition they had had since').messages,
+    retext().use(repeated).processSync('the most heartening exhibition they had had since').messages,
     [],
     'should ignore some valid repetitions'
   );
 
   t.deepEqual(
-    retext().use(repeated).process('The Mau Mau Uprising, also known as the Mau Mau Rebellion, Mau Mau Revolt, or Kenya Emergency, was a military conflict that took place in British Kenya').messages,
+    retext().use(repeated).processSync('The Mau Mau Uprising, also known as the Mau Mau Rebellion, Mau Mau Revolt, or Kenya Emergency, was a military conflict that took place in British Kenya').messages,
     [],
     'should ignore some valid repetitions (mau)'
   );
